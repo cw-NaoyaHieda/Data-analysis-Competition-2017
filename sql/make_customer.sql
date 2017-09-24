dc889de62ce3a892b7826a60d88ed7833862f63c
naoya <hero12185@gmail.com>
2017-09-24 22:54:23 +0900

    update make_customer.sql

---
+++ sql/make_customer.sql
@@ -0,0 +1,23 @@
+﻿-- Table: public.customer
+
+-- DROP TABLE public.customer;
+DROP TABLE IF EXISTS  public.customer;
+
+CREATE TABLE public.customer
+(
+
+customer_ID char(5),
+first_year int,
+first_store char(5),
+zip_code int,
+DM char(2),
+sex char(2),
+birth_age char(20),
+comment char(20)
+
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.customer
+  OWNER TO postgres;
