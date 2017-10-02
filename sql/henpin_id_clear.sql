c1d9da57d8ade940b9f0c7a2e4c34cef0b2a3589
takuto0831 <aseries2765@gmail.com>
2017-10-02 21:12:54 +0900

    会計の一致も追加

---
+++ sql/henpin_id_clear.sql
@@ -0,0 +1,44 @@
+﻿-- DROP TABLE public.customer;
+DROP TABLE IF EXISTS  receipt_henpin_syori;
+
+CREATE TABLE
+	receipt_henpin_syori
+-- 返品で抜き出したIDを消去する
+AS SELECT
+ 	receipt_id,
+	store_id,
+	dt,
+	t,
+	customer_id,
+	in_tax,
+	tax,
+	CASE trans_category WHEN '返品' THEN '入力ミス' END AS trans_category ,
+	pos_staff,
+	regi_staff,
+	simei,
+	coupon,
+	counpon_num,
+	cash,
+	credit,
+	ec_money,	
+	urikake,	
+	other_coupon,
+	other_coupon_num,
+	sum_coupon_num,
+	coupon_id,
+	use_point,
+	use_point_class,
+	point_grant,
+	point_balance,
+	cs_point	,
+	item_num
+FROM
+	receipt_1
+	left outer join
+	id_list
+		on receipt_1.receipt_id = id_list.henpin_receipt
+		or receipt_1.receipt_id = id_list.hanbai_receipt
+	where
+		id_list.henpin_receipt is NULL
+
+		
---
+++ sql/line_id_clear.sql
@@ -0,0 +1,26 @@
+﻿-- DROP TABLE public.customer;
+DROP TABLE IF EXISTS  line_henpin_syori;
+
+CREATE TABLE
+	line_henpin_syori
+-- 返品で抜き出したIDを消去する
+AS SELECT
+ 	line_id,
+	receipt_id,
+	item_treat,
+	in_tax,
+	discount,
+	tax,
+	product_id,
+	staff_id,
+	simei
+FROM
+	line_1
+	left outer join
+	id_list
+		on line_1.line_id = id_list.henpin_line_id
+		or line_1.line_id = id_list.hanbai_line_id
+	where
+	id_list.henpin_line_id is NULL
+
+		
