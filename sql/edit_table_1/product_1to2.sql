842423704a1d50367f90762cb36b9e547c929a71
kur0cky <blacky.kur0cky@gmail.com>
2017-10-02 20:57:51 +0900

    product_1to2.sql, adding

---
+++ sql/edit_table_1/product_1to2.sql
@@ -0,0 +1,10 @@
+﻿UPDATE
+	product_1
+SET
+	product_name = '新宿サウス店不明クーポン'
+WHERE
+	product_name = '#NAME?'; --商品名が#NAME?だったものを変更。
+				 --商品カテゴリなどを参照し
+
+
+ALTER TABLE product_1 RENAME TO product_2;
