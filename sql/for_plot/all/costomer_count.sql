567b4f7dfaf32a3ecc2754026651fc27861db931
稗田尚弥 <hero12185@gmail.com>
2017-10-04 12:04:12 +0900

    for_all plot

---
+++ sql/for_plot/all/costomer_count.sql
@@ -0,0 +1,14 @@
+﻿SELECT
+	sex
+	,birth_age
+	,count(*) AS count
+FROM
+	receipt_henpin_syori
+LEFT JOIN
+	customer_2
+ON
+	receipt_henpin_syori.customer_id = customer_2.customer_id
+GROUP BY
+	receipt_henpin_syori.customer_id,sex,birth_age
+ORDER BY
+	count DESC
---
+++ sql/for_plot/all/sex_age.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+	sex,
+	birth_age,
+	count(*)
+FROM
+	customer_2
+GROUP BY
+	sex,birth_age
+ORDER BY
+	sex,birth_age
