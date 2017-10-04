780a3304b49026743360ad751d1fe3cade9711d8
kur0cky <blacky.kur0cky@gmail.com>
2017-10-05 01:11:41 +0900

    line_henpin_syori_fin_1.sql adding

---
+++ sql/edit_table_1/line_henpin_syori_fin_1.sql
@@ -0,0 +1,12 @@
+﻿SELECT * from line_henpin_syori_fin 
+where staff_id = '#N/A';
+
+UPDATE
+	line_henpin_syori_fin 
+SET
+	staff_id = NULL
+WHERE
+	staff_id = '#N/A'; --担当者idが#N/AだったところをNULLに
+
+
+ALTER TABLE line_henpin_syori_fin RENAME TO line_henpin_syori_fin_1;
