b6629be25af4e222c2280c4e839b1331d4a2b821
takuto0831 <aseries2765@gmail.com>
2017-10-04 17:31:25 +0900

    first commit メモ用

---
+++ sql/henpin_table/memo.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+	*
+FROM 
+	receipt_1 AS A
+	LEFT JOIN
+		line_1 AS B
+	ON A.receipt_id = B.receipt_id
+	-- line_henpin_syori
+WHERE
+	A.receipt_id = 1580598 OR A.receipt_id = 1580595 OR A.receipt_id = 1580596
