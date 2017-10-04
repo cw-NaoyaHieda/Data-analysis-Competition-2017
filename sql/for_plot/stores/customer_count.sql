4c89aa7bfed8ef64434aed79fbd2278df4785df4
稗田尚弥 <hero12185@gmail.com>
2017-10-04 16:51:25 +0900

    for_plot stores and make_table gaikuinfo

---
+++ sql/for_plot/stores/customer_count.sql
@@ -0,0 +1,19 @@
+﻿SELECT
+	store_name
+	,sex
+	,birth_age
+	,count(*) AS count
+FROM
+	receipt_henpin_syori
+LEFT JOIN
+	customer_2
+ON
+	receipt_henpin_syori.customer_id = customer_2.customer_id
+LEFT JOIN
+	store_1
+ON
+	receipt_henpin_syori.store_id = store_1.store_id
+GROUP BY
+	store_name,receipt_henpin_syori.customer_id,sex,birth_age
+ORDER BY
+	store_name,count DESC
---
+++ sql/for_plot/stores/sex_age.sql
@@ -0,0 +1,15 @@
+﻿SELECT
+	store_name,
+	sex,
+	birth_age,
+	count(*)
+FROM
+	customer_2
+LEFT JOIN
+	store_1
+ON
+	customer_2.first_store = store_1.store_id
+GROUP BY
+	store_name,sex,birth_age
+ORDER BY
+	store_name,sex,birth_age
