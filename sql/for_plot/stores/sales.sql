8c9649b7594bcc24dcb75c01e4e411232a3df649
稗田尚弥 <hero12185@gmail.com>
2017-10-05 13:40:22 +0900

    first commit for my branch

--- sql/for_plot/stores/sales.sql
+++ sql/for_plot/stores/sales.sql
@@ -1,18 +1,18 @@
-﻿SELECT
+SELECT
 	store_1.store_name,
 	date_part('year',receipt_henpin_syori.dt) AS year,
 	date_part('month',receipt_henpin_syori.dt) AS month,
 	count(receipt_henpin_syori.receipt_id) AS customer_count,
-	sum(line_henpin_syori.in_tax - line_henpin_syori.tax) AS earn,
-	sum(CASE WHEN item_treat = '施術販売' THEN line_henpin_syori.in_tax - line_henpin_syori.tax ELSE 0 END) AS service,
-	sum(CASE WHEN item_treat = '物販商品' THEN line_henpin_syori.in_tax - line_henpin_syori.tax ELSE 0 END) AS item,
-	sum(line_henpin_syori.in_tax - line_henpin_syori.tax) / count(receipt_henpin_syori.receipt_id) AS one_person
+	sum(line_henpin_syori_fin_1.in_tax - line_henpin_syori_fin_1.tax) AS earn,
+	sum(CASE WHEN item_treat = '施術販売' THEN line_henpin_syori_fin_1.in_tax - line_henpin_syori_fin_1.tax ELSE 0 END) AS service,
+	sum(CASE WHEN item_treat = '物販商品' THEN line_henpin_syori_fin_1.in_tax - line_henpin_syori_fin_1.tax ELSE 0 END) AS item,
+	sum(line_henpin_syori_fin_1.in_tax - line_henpin_syori_fin_1.tax) / count(receipt_henpin_syori.receipt_id) AS one_person
 FROM
 	receipt_henpin_syori
 LEFT JOIN
-	line_henpin_syori
+	line_henpin_syori_fin_1
 ON
-	receipt_henpin_syori.receipt_id = line_henpin_syori.receipt_id
+	receipt_henpin_syori.receipt_id = line_henpin_syori_fin_1.receipt_id
 LEFT JOIN
 	store_1
 ON
---
+++ sql/make_table/gaiku_info.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE gaiku_infos (
+id integer NOT NULL PRIMARY KEY,
+pref_kanji varchar(36),
+city_kanji varchar(128),
+town_kanji varchar(128),
+number varchar(128),
+coordinate_no char(2),
+coordinate_x numeric,
+coordinate_y numeric,
+longitude numeric,
+latitude numeric,
+disp_flg char(1),
+represent_flg char(1),
+history_flg1 char(1),
+history_flg2 char(1)
+);
+CREATE INDEX idx_pref ON gaiku_infos (
+pref_kanji
+);
+CREATE INDEX idx_city ON gaiku_infos (
+city_kanji
+);
+CREATE INDEX idx_town ON gaiku_infos (
+town_kanji
+);
+CREATE INDEX idx_address ON gaiku_infos (
+pref_kanji,city_kanji,town_kanji
+);
