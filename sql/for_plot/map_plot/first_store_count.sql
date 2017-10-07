f876ccac349c64dc4109f96cad9b1b2c64a84247
稗田尚弥 <hero12185@gmail.com>
2017-10-07 18:34:59 +0900

    add plot_query

---
+++ sql/for_plot/map_plot/first_store_count.sql
@@ -0,0 +1,19 @@
+﻿SELECT
+	zip_geocodes_over5.zip_code,
+	customer_2.first_store,
+	count(*),
+	AVG(longitude) AS long,
+	AVG(latitude) AS lat
+	
+FROM
+	customer_2
+LEFT JOIN
+	zip_geocodes_over5
+ON
+	SUBSTR(TRIM(CAST(customer_2.zip_code AS character(10))),1,5) = zip_geocodes_over5.zip_code
+GROUP BY
+	zip_geocodes_over5.zip_code , customer_2.first_store
+HAVING
+	zip_geocodes_over5.zip_code IS NOT NULL
+ORDER BY
+	customer_2.first_store
