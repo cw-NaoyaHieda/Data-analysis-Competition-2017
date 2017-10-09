5dbb0a62066a51b6a45faf8d460c66f7264d1ce1
kur0cky <blacky.kur0cky@gmail.com>
2017-10-09 18:27:14 +0900

    viewの追加　staff_monthly

---
+++ sql/create_view/staff_monthly.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW staff_monthly AS 
+
+SELECT 
+	regi_staff, date_trunc('month',dt), COUNT(*)
+FROM 
+	receipt_1
+GROUP BY
+	regi_staff, date_trunc('month',dt)
+ORDER BY
+	regi_staff, date_trunc('month',dt);
