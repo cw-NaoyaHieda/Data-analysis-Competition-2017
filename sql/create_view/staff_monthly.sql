b550e85480285440e7138a4e7f712ebb3940dcfa
kur0cky <blacky.kur0cky@gmail.com>
2017-10-10 14:54:23 +0900

    staff_monthly.sql

--- sql/create_view/staff_monthly.sql
+++ sql/create_view/staff_monthly.sql
@@ -1,10 +1,15 @@
-﻿CREATE VIEW staff_monthly AS 
+﻿-- View: public.staff_monthly
 
-SELECT 
-	regi_staff, date_trunc('month',dt), COUNT(*)
-FROM 
-	receipt_1
-GROUP BY
-	regi_staff, date_trunc('month',dt)
-ORDER BY
-	regi_staff, date_trunc('month',dt);
+-- DROP VIEW public.staff_monthly;
+
+CREATE OR REPLACE VIEW public.staff_monthly AS 
+ SELECT receipt_henpin_syori.regi_staff,
+    date_trunc('month'::text, receipt_henpin_syori.dt::timestamp with time zone) AS date_trunc,
+    count(*) AS all_count,
+    count(simei = '指名') AS only_simei_count
+   FROM receipt_henpin_syori
+  GROUP BY receipt_henpin_syori.regi_staff, (date_trunc('month'::text, receipt_henpin_syori.dt::timestamp with time zone))
+  ORDER BY receipt_henpin_syori.regi_staff, (date_trunc('month'::text, receipt_henpin_syori.dt::timestamp with time zone));
+
+ALTER TABLE public.staff_monthly
+  OWNER TO postgres;
